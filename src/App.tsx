39f8cfa68dbf4b7ca871f696686539816e728760
mark.avvakumov <maavvakumov@gmail.com>
2023-11-01 19:15:47 +0700

    Added useUsers hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import './App.css';
+
 import { useState } from 'react';
+import { useUsers } from './components/hooks/useUsers';
 
 import AppLayout from './components/AppLayout';
 import Search from './components/Search';
 import UserCardList from './components/UserCardList';
 import UserCard from './components/UserCard';
-import './App.css';
 
 function App() {
   const [query, setQuery] = useState('');
+  const { users, isLoading, error } = useUsers();
+
   return (
     <AppLayout>
       <Search query={query} setQuery={setQuery} />
-      <UserCardList>
+      <UserCardList users={users}>
         <UserCard />
       </UserCardList>
     </AppLayout>
