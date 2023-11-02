23640a1632f7b625328d8371b93a9ed818023235
mark.avvakumov <maavvakumov@gmail.com>
2023-11-02 15:37:45 +0700

    Added Modal

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,25 @@ import { useState } from 'react';
 import AppLayout from './components/AppLayout';
 import Search from './components/Search';
 import UserCardList from './components/UserCardList';
+import Modal from './components/Modal';
 
 function App() {
   const [query, setQuery] = useState('');
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(!isModalOpen);
+  };
 
   return (
     <AppLayout>
       <Search query={query} setQuery={setQuery} />
-      <UserCardList />
+      <UserCardList openModal={openModal} />
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
     </AppLayout>
   );
 }
